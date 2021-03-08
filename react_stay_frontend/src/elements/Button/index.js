41c7aec5859bc2e23ea8b2f598a92b8fdf7517a3
celvineadiputra_ <celvineadiputra2@gmail.com>
2021-03-08 15:02:20 +0700

    01. Button

--- react_stay_frontend/src/elements/Button/index.js
+++ react_stay_frontend/src/elements/Button/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import propTypes from "prop-types";
 
 export default function Button(props) {
   return <div></div>;
 }
+
+Button.propTypes = {
+  type: propTypes.oneOf(["button", "link"]), //hanya terima button atau link
+  onClick: propTypes.func,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: props.bool,
+  isLoading: props.bool,
+  isSmall: props.bool,
+  isLarge: props.bool,
+  isBlock: props.bool,
+  hasShadow: props.bool,
+};
